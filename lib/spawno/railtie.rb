d363cf846712614ebf3810a675de867eecce23af
Dieter Pisarewski <dieter.pisarewski@gmail.com>
2014-12-05 17:42:14 +0100

    trigger rails initializer only on server startup

--- lib/spawno/railtie.rb
+++ lib/spawno/railtie.rb
@@ -1,7 +1,7 @@
 module Spawno
   class Railtie < ::Rails::Railtie
     initializer 'spawno' do |app|
-      Spawno.setup(app.root)
+      Spawno.setup(app.root) if defined?(Rails::Server)
     end
   end
 end
